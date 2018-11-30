cd9ed08ba40b75783ce7791e54aeff15d176f9fc
DaioneB <dcbpavan@restinga.ifrs.edu.br>
2018-11-30 07:26:26 -0200

    organizando o projeto pra colocar o bootstrap

---
+++ floricultura-front/src/plantas/Plantas.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Plantas extends Component{
+    constructor(){
+        super(); 
+
+    }
+
+    render(){
+        return<h1>Helo World</h1>; 
+    }
+
+}
